cd6ef3cd748adf17e47e4dcceff43bc5b2c42467
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-09-02 20:15:10 +0900

    feat: implement basic functions (#1)
    * feat: implement basic functions
    
    * chore(aqua): update aqua/aqua-checksums.json
    
    * fix: fix location
    
    * debug
    
    * fix: fix working directory of reviewdog
    
    * Revert "debug"
    
    This reverts commit dd2de4b363fa014343fd81a57060ad73a2ba6f22.
    
    * docs: update README
    
    ---------
    
    Co-authored-by: suzuki-shunsuke-app[bot] <91834585+suzuki-shunsuke-app[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# github-action-trivy-config
-GitHub Actions for trivy config
+# trivy-config-action
+
+GitHub Actions for [trivy config](https://aquasecurity.github.io/trivy/latest/docs/references/configuration/cli/trivy_config/) command
+
+![image](https://github.com/suzuki-shunsuke/trivy-config-action/assets/13323303/e4d7f6f7-3df3-44bb-8f98-535173ce096e)
+
+--
+
+![image](https://github.com/suzuki-shunsuke/trivy-config-action/assets/13323303/2d0c6224-8ae4-42f0-80d8-06488ff18f56)
+
+Run `trivy config` and notify the result with [reviewdog](https://github.com/reviewdog/reviewdog) and [github-comment](https://github.com/suzuki-shunsuke/github-comment).
+
+This GitHub Actions does **not** install trivy and reviewdog, so you have to install them in advance.
+It allows to install tools outside this action.
+[aqua](https://aquaproj.github.io/) is useful to install them.
+
+## Requirements
+
+- [trivy](https://github.com/aquasecurity/trivy)
+- [reviewdog](https://github.com/reviewdog/reviewdog)
+- (Optional) [github-comment](https://github.com/suzuki-shunsuke/github-comment)
+
+## Usage
+
+```yaml
+- uses: suzuki-shunsuke/trivy-config-action@main
+  with:
+    github_token: ${{github.token}}
+    working_directory: foo
+    github_comment: 'true'
+```
+
+## Inputs
+
+### Required Inputs
+
+Nothing.
+
+### Optional Inputs
+
+name | default value | description
+--- | --- | ---
+github_token | `github.token` | GitHub Access Token
+working_directory | "" (current directory) | Woring Directory
+github_comment | `false` | Whether a comment is posted with github-comment
+
+## Outputs
+
+Nothing.
+
+## LICENSE
+
+[MIT](LICENSE)
---
+++ src/run.ts
@@ -0,0 +1,160 @@
+import * as exec from '@actions/exec';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as path from 'path';
+
+type Inputs = {
+  workingDirectory: string
+  githubToken: string
+  githubComment: boolean
+}
+
+class DiagnosticCode {
+  value = ''
+  url = ''
+}
+
+class DiagnosticLocation {
+  path = ''
+  range = new DiagnosticLocationRange()
+}
+
+class DiagnosticLocationRange {
+  start = new DiagnosticLocationRangePoint()
+  end = new DiagnosticLocationRangePoint()
+}
+
+class DiagnosticLocationRangePoint {
+  line = 0
+}
+
+class Diagnostic {
+  message = ''
+  code = new DiagnosticCode()
+  location = new DiagnosticLocation()
+  severity = ''
+}
+
+function generateTable(diagnostics: Array<Diagnostic>, basePath: string): string {
+  const lines: Array<string> = ['rule | severity | filepath | range | message', '--- | --- | --- | --- | ---'];
+  for (let i = 0; i < diagnostics.length; i++) {
+    const diagnostic = diagnostics[i];
+
+    let rule = diagnostic.code.value;
+    if (diagnostic.code.url) {
+      rule = `[${diagnostic.code.value}](${diagnostic.code.url})`;
+    }
+
+    let range = '';
+    if (diagnostic.location && diagnostic.location.range && diagnostic.location.range.start) {
+      range = `${diagnostic.location.range.start.line} ... ${diagnostic.location.range.end.line}`;
+    }
+
+    let locPath = diagnostic.location.path;
+    if (path.isAbsolute(diagnostic.location.path)) {
+      locPath = path.relative(basePath, diagnostic.location.path);
+    }
+
+    lines.push(`${rule} | ${diagnostic.severity} | ${locPath} | ${range} | ${diagnostic.message}`);
+  }
+  return lines.join('\n');
+}
+
+function getSeverity(s: string): string {
+  if (s.startsWith('HIGH') || s.startsWith('CRITICAL')) {
+    return 'ERROR';
+  }
+  if (s.startsWith('MEDIUM')) {
+    return 'WARNING';
+  }
+  if (s.startsWith('LOW')) {
+    return 'INFO';
+  }
+  return '';
+}
+
+function getURL(result: any): string {
+  if (result.links && result.links.length != 0) {
+    return result.links[0];
+  }
+  return '';
+}
+
+export const run = async (inputs: Inputs): Promise<void> => {
+  core.info('Running trivy config');
+  const args = ['config', '--format', 'json', '.'];
+  const out = await exec.getExecOutput('trivy', args, {
+    cwd: inputs.workingDirectory,
+    ignoreReturnCode: true,
+  });
+  core.info('Parsing trivy config result');
+  const outJSON = JSON.parse(out.stdout);
+  if (outJSON.Results == null) {
+    core.info('trivy config is null');
+    return;
+  }
+  const diagnostics = new Array<Diagnostic>();
+  for (let i = 0; i < outJSON.Results.length; i++) {
+    const result = outJSON.Results[i];
+    if (result.Misconfigurations == null) {
+      continue;
+    }
+    for (let j = 0; j < result.Misconfigurations.length; j++) {
+      const misconfig = result.Misconfigurations[j];
+      diagnostics.push({
+        message: misconfig.Message,
+        code: {
+          value: misconfig.ID,
+          url: misconfig.PrimaryURL,
+        },
+        location: {
+          path: result.Target,
+          range: {
+            start: {
+              line: misconfig.CauseMetadata.StartLine,
+            },
+            end: {
+              line: misconfig.CauseMetadata.EndLine,
+            },
+          },
+        },
+        severity: getSeverity(misconfig.Severity),
+      });
+    }
+  }
+
+  if (inputs.githubComment && diagnostics.length > 0) {
+    const table = generateTable(diagnostics, inputs.workingDirectory);
+    const githubCommentTemplate = `## :x: Trivy error
+
+{{template "link" .}} | [trivy](https://aquasecurity.github.io/trivy)
+
+Working Directory: \`${inputs.workingDirectory}\`
+
+${table}`;
+    await exec.exec('github-comment', ['post', '-stdin-template'], {
+      input: Buffer.from(githubCommentTemplate),
+      env: {
+        ...process.env,
+        GITHUB_TOKEN: inputs.githubToken,
+      },
+    });
+  }
+
+  const reporter = github.context.eventName == 'pull_request' ? 'github-pr-review' : 'github-check';
+  core.info('Running reviewdog');
+  await exec.exec('reviewdog', ['-f', 'rdjson', '-name', 'trivy', '-filter-mode', 'nofilter', '-reporter', reporter, '-level', 'warning', '-fail-on-error', '1'], {
+    input: Buffer.from(JSON.stringify({
+      source: {
+        name: "trivy",
+        url: "https://github.com/aquasecurity/trivy"
+      },
+      diagnostics: diagnostics,
+    })),
+    cwd: inputs.workingDirectory,
+    env: {
+      ...process.env,
+      REVIEWDOG_GITHUB_API_TOKEN: inputs.githubToken,
+    },
+  });
+}
